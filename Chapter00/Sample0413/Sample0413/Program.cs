6cdc982225cf3ddd24b751bc3024c9a358351881
infosys <infosys@C4216>
2026-04-20 09:34:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter00/Sample0413/Sample0413/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample0413
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("表示回数：");
+            string?  inputNum = Console.ReadLine(); //入力
+
+            int count = int.Parse(inputNum);
+            int i = 1;
+            while(i <= count){
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + ":");
+                    Console.WriteLine("群馬県太田市"); //画面出力
+                }
+                i++;
+            } 
+        }
+    }
+}
---
+++ Chapter00/Sample0415/Sample0415/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] hairetu = new int[10];
+            for (int i = 0; i < hairetu.Length; i++)
+            {
+                Console.Write("hairetu[" + i + "] =");
+                string? inputNum = Console.ReadLine();
+                int num = int.Parse(inputNum);
+                hairetu[i] = num;
+            }
+
+
+            //for (int i = 0; i < hairetu.Length; i++)
+            //{
+            //    sum = hairetu[i] + sum;
+            //}
+            for (int i = 0; i < hairetu.Length; i++)
+            {
+                Console.Write("hairetu[" + i + "] : ");
+                astOut(hairetu[i]);
+            }
+
+            //for (int i = 0; i < hairetu.Length; i++)
+            //{
+            //    Console.WriteLine("hairetu[" + i + "] = " + hairetu[i]);
+            //}
+            Console.WriteLine("合計:" + hairetu.Where(n=>n % 2 ==0).Sum());
+        }
+
+        static void astOut(int numm)
+        {
+            for (int j = 0; j < numm; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();         
+            
+        }
+    }
+
+}
+
+
+

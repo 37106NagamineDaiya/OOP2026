bd4f52ac49ebaea69f54aa560d9a4eaa42e89fad
大也 永峰 <oibc37106@ojs.ac.jp>
2026-05-07 11:19:20 +0900

    SalesCounterクラスの変更

--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -9,17 +9,37 @@ namespace SalesCalculator {
     public class SalesCounter {
         private readonly List<Sale> _sales;
 
+
+
         //コンストラクター
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
+
+        public List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+
+                };
+                sales.Add(sale);
+            }
+            return sales;
+
+        }
+
 
         //店舗別売り上げを求める
         public Dictionary<string, int> GetPerStoreSale() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
                 //既に店舗名が辞書のキーに登録されているか？
-                if (dict.ContainsKey(sale.ShopName)) 
+                if (dict.ContainsKey(sale.ShopName))
                     //登録されている場合
                     dict[sale.ShopName] += sale.Amount; //売上を足しこみ
                 else

66baba490ae3743656cd320eece2312bbafc13da
大也 永峰 <oibc37106@ojs.ac.jp>
2026-05-13 11:16:59 +0900

    2-2完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class InchConverter {
+        public static readonly double raito = 0.0254;
+        public static double ToMeter(double inch) {
+            return inch * raito;
+
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            PrintInchToMeterList(1, 10);
+        }
+
+        private static void PrintInchToMeterList(int start, int end) {
+            for(int inch = start; inch <= end; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine($"{inch}″ = {meter}m");
+            }
+        }
+
+
+
+    }
+}
+

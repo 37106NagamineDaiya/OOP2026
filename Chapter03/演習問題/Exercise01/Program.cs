b1e4033a042c38cb8b5cab1ef790caeb34797678
大也 永峰 <oibc37106@ojs.ac.jp>
2026-05-21 09:48:59 +0900

    問題3.1.1完成

---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise4(numbers);
+        }
+
+        private static void Exercise1(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.1完成」
+            var exists = numbers.Exists((s => s % 8 == 0 || s % 9 == 0));
+            Console.WriteLine(exists);
+        }
+
+        private static void Exercise2(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.2完成」
+        }
+
+        private static void Exercise3(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.3完成」
+        }
+
+        private static void Exercise4(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.4完成」
+        }
+    }
+}

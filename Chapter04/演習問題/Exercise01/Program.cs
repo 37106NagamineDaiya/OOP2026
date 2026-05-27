95bedb73e1eaa5637c4c684dea373c1728dd0610
大也 永峰 <oibc37106@ojs.ac.jp>
2026-05-27 09:47:15 +0900

    4.1.1完成

---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<string> langs = [
+                "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+
+        }
+
+        private static void Exercise1(List<string> langs) {
+            Console.WriteLine("foreach文");
+
+            var names = langs.FindAll(s => s.Contains('S'));
+            foreach (var s in names) {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("\nfor文");
+            names = langs.FindAll(s => s.Contains('S'));
+            for(var i = 0; i < names.Count; i++) {
+                Console.WriteLine(names[i]);
+            }
+
+
+
+
+            Console.WriteLine("\nwhile文");
+            names = langs.FindAll(s => s.Contains('S'));
+            var j = 0;
+            while (j < names.Count) {
+                Console.WriteLine(names[j]);
+                j++;
+
+            }
+
+
+        }
+
+        private static void Exercise2(List<string> langs) {
+            
+        }
+
+        private static void Exercise3(List<string> langs) {
+            
+        }
+    }
+}

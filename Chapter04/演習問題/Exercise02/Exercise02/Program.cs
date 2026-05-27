f41084d5906bd1b106cfd3b9fa9075f663ca3191
大也 永峰 <oibc37106@ojs.ac.jp>
2026-05-27 11:22:12 +0900

    4.2.1完成

---
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.ComponentModel.Design;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" --- 4.2.1 ---");
+            Exercise1();
+            Console.WriteLine("\n --- 4.2.2 ---");
+            Exercise2();
+            Console.WriteLine("\n --- 4.2.3 ---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            //if-else文を使用
+            Console.Write("数値を入力");
+            var num = Console.ReadLine();
+            bool result = int.TryParse(num, out int number);
+            if (result) {
+                int nums = int.Parse(num);
+                if (nums >= 500) {
+                    Console.WriteLine(nums);
+                } else if (nums >= 100 && nums < 500) {
+                    Console.WriteLine(nums * 3);
+                } else if (nums >= 0 && nums < 100) {
+                    Console.WriteLine(nums * 2);
+                } else {
+                    Console.WriteLine(nums);
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
+        }
+
+        private static void Exercise2() {
+            //switch文を使用
+
+        }
+
+        private static void Exercise3() {
+            //switch式を使用
+
+
+        }
+    }
+}

f09776d8438845cc552fa0c599d491db9eda3852
大也 永峰 <oibc37106@ojs.ac.jp>
2026-06-08 11:18:19 +0900

    5章終了

---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exercise01;
+using System.Security.Cryptography.X509Certificates;
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var ym1 = new YearMonth(2010, 5);
+            var ym2 = new YearMonth(2010, 5);
+             if(ym1 == ym2) {
+                Console.WriteLine("ym1とym2は等しい");
+            } else {
+                Console.WriteLine("ym1とym2は等しくない");
+            }
+
+        }
+
+    }
+}
